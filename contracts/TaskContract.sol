9bc142392fd5fa7c6c56a98ca345f23f719c4fdb
FCryptoFx <ferrancryptofx@gmail.com>
2023-03-03 14:07:03 +0100

    add creat and read part from CRUD

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -4,4 +4,32 @@ pragma solidity ^0.8.6;
 
 contract TaskContract{
     
+    uint nextId;
+
+    struct Task {
+        uint id;
+        string name;
+        string description;
+    }
+
+    Task [] tasks;
+
+    function createTask(string memory _name, string memory _description) public{
+        tasks.push(Task (nextId, _name, _description));
+        nextId ++;
+    }
+
+    function findIndex (uint _id) internal view returns (uint) {
+        for(uint i = 0; i<tasks.length; i++){
+            if(tasks[i].id == _id){
+                return i;
+            }
+        }
+        revert('Task not found');
+    }
+
+    function readTask(uint _id) public view returns (uint, string memory, string memory){
+        uint index = findIndex(_id);
+        return (tasks[index].id, tasks[index].name, tasks[index].description);
+    }
 }
